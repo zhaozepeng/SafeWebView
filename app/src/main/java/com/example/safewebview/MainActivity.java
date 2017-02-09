5ddb98c0a8a429aba295724c00d97b85a62baaba
zhaozepeng <zhaozepeng@users.noreply.github.com>
2017-02-09 11:52:56 +0800

    demo 完成

--- app/src/main/java/com/example/safewebview/MainActivity.java
+++ app/src/main/java/com/example/safewebview/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -11,5 +13,46 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    SafeWebView mWebView;
+    //	SafeWebView mWebView;
+    String mUrl2 = "file:///android_asset/test.html";
+    String mUrl3 = "file:///android_asset/js3.html";
+    Object mJsObj = new JSInterface();
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (SafeWebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.addJavascriptInterface(new JSInterface(), "jsInterface");
+        mWebView.loadUrl(mUrl2);
+    }
+
+    class JSInterface {
+        @JavascriptInterface
+        public String onButtonClick(String text) {
+            final String str = text;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), "onButtonClick: text = " + str, Toast.LENGTH_LONG).show();
+                }
+            });
+
+            return "This text is returned from Java layer.  js text = " + text;  //return数据在JS中alert显示
+        }
+
+        @JavascriptInterface
+        public void onImageClick(String url, int width, int height) {
+            final String str = "onImageClick: text = " + url + "  width = " + width + "  height = " + height;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
 }
